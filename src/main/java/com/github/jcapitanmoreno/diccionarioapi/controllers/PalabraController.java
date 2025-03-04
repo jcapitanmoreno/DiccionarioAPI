a11532d36f2e8625281fefa00f88e714bd6c63a8
jcapitanmoreno <jcapitanmoreno@gmail.com>
2025-03-04 13:55:15 +0100

    buscar por definiciones y sin definiciones

--- src/main/java/com/github/jcapitanmoreno/diccionarioapi/controllers/PalabraController.java
+++ src/main/java/com/github/jcapitanmoreno/diccionarioapi/controllers/PalabraController.java
@@ -41,9 +41,21 @@ public class PalabraController {
     }
 
     @CrossOrigin
-    @GetMapping("/{id}")
+    @GetMapping("/{id}/definiciones")
     public ResponseEntity<Palabra> getPalabraById(@PathVariable Long id) throws RecordNotFoundException {
         Palabra palabra = palabraService.getPalabraById(id);
+        return new ResponseEntity<Palabra>(palabra, new HttpHeaders(), HttpStatus.OK);
+    }
+
+    @CrossOrigin
+    @GetMapping("/{id}")
+    public ResponseEntity<Palabra> getPalabraPorIdSinDefiniciones(@PathVariable Long id) throws RecordNotFoundException { //con la anotación @PathVariable le estoy diciendo que este argumento es el dato que va a poner cliente en la ruta (id)
+        Palabra palabra = palabraService.getPalabraById(id);
+        if (palabra == null) {
+            throw new RecordNotFoundException("No se ha encontrado la palabra",id);
+        }
+        palabra.setDefiniciones(null);
+
         return new ResponseEntity<Palabra>(palabra, new HttpHeaders(), HttpStatus.OK);
     }
 
